560934227bdbaac9df0a4a8abf39c25264560a5c
unknown <Lankin-03@mail.ru>
2022-03-10 18:11:45 +0400

    Добавил в Hello_world функцию 'fib(n)' вычисляющую n-ый член последовательности Фибоначчи, считая, что она начинается с (1, 1, ...)

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int fib(int n){
+    if(n < 3){
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main(){
     cout << "Hello world!" << endl;
+    int n;
+    cout << "¬ведите номер члена последовательности:" << endl;
+    cin >> n;
+    cout << fib(n);
 }
